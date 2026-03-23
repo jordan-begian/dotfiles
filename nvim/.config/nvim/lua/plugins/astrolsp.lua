c8346c407dbc240b4b0d26ae69b69900ae9face3
Jordan Begian <40347351+jordan-begian@users.noreply.github.com>
2026-03-23 11:20:50 -0400

    add love2d support to lua_ls LSP

--- nvim/.config/nvim/lua/plugins/astrolsp.lua
+++ nvim/.config/nvim/lua/plugins/astrolsp.lua
@@ -43,11 +43,21 @@ return {
     ---@diagnostic disable: missing-fields
     config = {
       -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
+      lua_ls = {
+        settings = {
+          Lua = {
+            workspace = {
+              library = {
+                "${3rd}/love2d/library",
+              },
+            },
+          },
+        },
+      },
     },
     -- customize how language servers are attached
     handlers = {
-      -- a function without a key is simply the default handler, functions take two parameters, the server name and the configured options table for that server
-      -- function(server, opts) require("lspconfig")[server].setup(opts) end
+      -- a function without a key is simply the default handler, functions take two parameters, the server name and the configured options table for that server function(server, opts) require("lspconfig")[server].setup(opts) end
 
       -- the key is the server that is being setup with `lspconfig`
       -- rust_analyzer = false, -- setting a handler to false will disable the set up of that language server
