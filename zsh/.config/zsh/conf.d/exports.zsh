4306a02f9de8dd3c35e6222d091dcbcdde942404
Jordan Begian <40347351+jordan-begian@users.noreply.github.com>
2026-03-23 17:39:14 -0400

    add hurl to $PATH

--- zsh/.config/zsh/conf.d/exports.zsh
+++ zsh/.config/zsh/conf.d/exports.zsh
@@ -1,7 +1,7 @@
 # Environment variables and PATH
 
 # If you come from bash you might have to change your $PATH.
-export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
+export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:/usr/bin/hurl:$PATH
 
 export MANPATH="/usr/local/man:$MANPATH"
 export LANG=en_US.UTF-8
